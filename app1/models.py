ac5c8b5941c739a2b09ada81885fe23657f03da0
Arjun T A <arjunta32@gmail.com>
2025-06-11 23:36:12 +0530

    fully completed db schema

--- app1/models.py
+++ app1/models.py
@@ -2,7 +2,6 @@ from django.db import models
 import uuid
 
 # Create your models here.
-
 class Industry(models.Model):
     id= models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     industry_name= models.CharField(max_length=100)
@@ -17,7 +16,7 @@ class Industry(models.Model):
 class Niche(models.Model):
     id= models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     niche_name= models.CharField(max_length=100)
-    industry_id= models.ForeignKey(Industry, on_delete=models.CASCADE)
+    industry= models.ForeignKey(Industry, on_delete=models.CASCADE)
 
     
     def __str__(self):
@@ -30,41 +29,7 @@ class Niche(models.Model):
 class Certifications(models.Model):
     id= models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     certificate_name= models.CharField(max_length=300)
-    niche_id= models.ForeignKey(Niche,on_delete=models.CASCADE)
-    website_link_url= models.URLField() 
-    certificate_logo= models.URLField() #s3 image url
-    cost_of_exam= models.FloatField()
-
-# Create your models here.
-
-class Industry(models.Model):
-    id= models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
-    industry_name= models.CharField(max_length=100)
-    
-    def __str__(self):
-        return self.industry_name
-    
-    class Meta:
-        db_table = 'industry'
-    
-
-class Niche(models.Model):
-    id= models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    niche_name= models.CharField(max_length=100)
-    industry_id= models.ForeignKey(Industry, on_delete=models.CASCADE)
-
-    
-    def __str__(self):
-        return self.niche_name
-    
-    class Meta:
-        db_table = 'niche'
-    
-
-class Certifications(models.Model):
-    id= models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
-    certificate_name= models.CharField(max_length=300)
-    niche_id= models.ForeignKey(Niche,on_delete=models.CASCADE)
+    niche= models.ForeignKey(Niche,on_delete=models.CASCADE)
     website_link_url= models.URLField() 
     certificate_logo= models.URLField() #s3 image url
     cost_of_exam= models.FloatField()
@@ -88,39 +53,7 @@ class Certifications(models.Model):
 
 class CertificationCourses(models.Model):
     id= models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
-    certification_id= models.ForeignKey(Certifications,on_delete=models.CASCADE)
-    course_title= models.CharField(max_length=500)
-    course_link= models.URLField()
-    cost= models.FloatField()
-    
-    def __str__(self):
-        return self.course_title    
-    
-    class Meta:
-        db_table = 'course'
-        
-
-    difficulty_level= models.CharField(max_length=100)
-    salary_hike= models.FloatField()
-    pass_percent= models.FloatField()
-    description= models.TextField()
-    avg_prep_duration= models.FloatField()
-    student_rating= models.FloatField()
-    exam_duration= models.FloatField()
-    issued_by= models.CharField(max_length=200)
-    validity= models.IntegerField()
-    exam_format= models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.certificate_name
-    
-    class Meta:
-        db_table = 'certification'
-
-
-class CertificationCourses(models.Model):
-    id= models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
-    certification_id= models.ForeignKey(Certifications,on_delete=models.CASCADE)
+    certification= models.ForeignKey(Certifications,on_delete=models.CASCADE)
     course_title= models.CharField(max_length=500)
     course_link= models.URLField()
     cost= models.FloatField()
