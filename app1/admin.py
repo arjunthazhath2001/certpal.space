487af36f20c76bf1c6f57fb3a2728b8458206cf9
Arjun T A <arjunta32@gmail.com>
2025-06-11 22:56:22 +0530

    defined db schemas

--- app1/admin.py
+++ app1/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from . models import *
 # Register your models here.
+
+admin.site.register(Industry)
+admin.site.register(Niche)
+admin.site.register(Certifications)
+admin.site.register(CertificationCourses)
+
+
--- app1/models.py
+++ app1/models.py
@@ -4,20 +4,63 @@ from django.db import models
 
 class Industry(models.Model):
     id= models.UUIDField()
-    name= models.CharField(max_length=100)
+    industry_name= models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.industry_name
+    
+    class Meta:
+        db_table = 'industry'
     
 
 class Niche(models.Model):
     id= models.UUIDField()
-    name= models.CharField(max_length=100)
+    niche_name= models.CharField(max_length=100)
     industry_id= models.ForeignKey(Industry, on_delete=models.CASCADE)
     total_no_of_certifications= models.IntegerField()
-
-
-class Certifcations(models.Model):
-    id= models.UUIDField()
-    name= models.CharField(max_length=300)
-    niche_id= models.ForeignKey(Niche,on_delete=models.CASCADE)
-    website_link_url= models.URLField()
-    certificate_logo= models.URLField() #s3 image url
     
+    def __str__(self):
+        return self.niche_name
+    
+    class Meta:
+        db_table = 'niche'
+    
+
+class Certifications(models.Model):
+    id= models.UUIDField()
+    certificate_name= models.CharField(max_length=300)
+    niche_id= models.ForeignKey(Niche,on_delete=models.CASCADE)
+    website_link_url= models.URLField() 
+    certificate_logo= models.URLField() #s3 image url
+    cost_of_exam= models.IntegerField()
+    difficulty_level= models.CharField(max_length=100)
+    salary_hike= models.IntegerField()
+    pass_percent= models.IntegerField()
+    description= models.TextField()
+    avg_prep_duration= models.IntegerField()
+    student_rating= models.IntegerField()
+    exam_duration= models.IntegerField()
+    issued_by= models.CharField(max_length=200)
+    validity= models.IntegerField()
+    exam_format= models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.certificate_name
+    
+    class Meta:
+        db_table = 'certification'
+
+
+class CertificationCourses(models.Model):
+    id= models.UUIDField()
+    certification_id= models.ForeignKey(Certifications,on_delete=models.CASCADE)
+    course_title= models.CharField(max_length=500)
+    course_link= models.URLField()
+    cost= models.IntegerField()
+    
+    def __str__(self):
+        return self.course_title    
+    
+    class Meta:
+        db_table = 'course'
+        
