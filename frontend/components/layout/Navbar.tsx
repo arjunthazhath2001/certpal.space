42d02da4a34ee066466dbc842a9d09dc2e0e8f24
Arjun T A <arjunta32@gmail.com>
2025-06-22 22:40:10 +0530

    Convert frontend from submodule to regular folder

---
+++ frontend/components/layout/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="bg-red-500">
+        <h1 className='font-bold text-white text-4xl'>
+            certpal
+        </h1>
+    </div>
+  )
+}
+
+export default Navbar
